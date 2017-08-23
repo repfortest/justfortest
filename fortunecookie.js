9cc57ad713325b27ba1ec18845394f672f3f5f16
repfortest <31272727+repfortest@users.noreply.github.com>
2017-08-23 12:51:22 +0300

    Update fortunecookie.js

--- fortunecookie.js
+++ fortunecookie.js
@@ -82,20 +82,8 @@ function FindAndChoose() {
 
 
             });
-
-
-        });
-      
-      
-      		var http = require('http');
-			http.createServer(function (req, res) {
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			res.end('Hello!');
-			}).listen(8080);
-      
-                
+        });     
     });
-    
 };  
 
 
